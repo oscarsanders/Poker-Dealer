e862b70729ac8f42fec97516b1208cd77242c449
oscarsanders <49376381+oscarsanders@users.noreply.github.com>
2023-04-11 01:29:48 -0700

    Add files via upload

--- poker_dealer.py
+++ poker_dealer.py
@@ -1,27 +1,34 @@
 import random
 
+r = 0
 while True:
-  print("<3 heart | => spades | =3 trebol | <> diamonds ")
-  print("Here is your hand, who win?")
+  r += 1
+  print("==================================================")
+  print(" | ♥ heart | ♠ spades | ♣ clubs | ♦ diamonds |")
+  print("==================================================")
+  print(f"Here is your hand, who win? Round: {r}")
   cards_num = ['A','K','Q','J','10','9','8','7','6','5','4','3','2']
 
-  cards_hearts = [(sym + ' ' + '<3') for sym in cards_num]
-  cards_spades = [(sym + ' ' + '=>') for sym in cards_num]
-  cards_trebol = [(sym + ' ' + '=3') for sym in cards_num]
-  cards_diamonds = [(sym + ' ' + '<>') for sym in cards_num]
+  cards_hearts = [(sym + ' ' + '♥') for sym in cards_num]
+  cards_spades = [(sym + ' ' + '♠') for sym in cards_num]
+  cards_clubs = [(sym + ' ' + '♣') for sym in cards_num]
+  cards_diamonds = [(sym + ' ' + '♦') for sym in cards_num]
 
-  cards = cards_diamonds + cards_hearts + cards_spades + cards_trebol
+  cards = cards_diamonds + cards_hearts + cards_spades + cards_clubs
 
   # mix cards
   random.shuffle(cards)
 
   user_cards = cards[:5]
   cpu_cards = cards[5:10]
-      
-  print(f"Your cards: {user_cards}")
-  print(f"CPU cards: {cpu_cards}")
-  finish = input("Want to finish? \
-    \nPress F or press Enter to continue: ")
+  
+  print("**************************************************")    
+  print(f"* Your cards: {user_cards}")
+  print("**************************************************")
+  print(f"* CPU cards : {cpu_cards}")
+  print("**************************************************")
+  finish = input("Want to finish?\
+     Press F \n Press Enter to continue: ")
   if finish.upper() == "F":
     break
 
